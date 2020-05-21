0f2e280710f43c1215bf580c235a8a206172844b
WillV1 <wmvanhook@gmail.com>
2020-05-21 18:28:49 -0400

    Updated api.js

--- Develop/api.js
+++ Develop/api.js
@@ -7,44 +7,62 @@ const writeFileSync = util.promisify(fs.writeFile);
 
 
 //  let note = fs.readFileSync ('.db/db/json', 'utf8')
-module.exports = function(app) {
+module.exports = function (app) {
 
-app.get("/api/notes", function (req, res) {
+  app.get("/api/notes", function (req, res) {
 
-  let noteList = (readFileSync('./db/db.json', 'utf8'))
-  // let getFileData = getFile();
-  // console.log(getFileData);
-  .then(noteList => res.json(JSON.parse(noteList)))
-    .catch(err => console.log(err))
-    console.log(noteList);
-});
+    (readFileSync('./db/db.json', 'utf8'))
+      // let getFileData = getFile();
+      // console.log(getFileData);
+      .then(noteList => {
+        console.log(noteList)
+        res.json(JSON.parse(noteList))
+      })
+      .catch(err => console.log(err))
 
-//   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, 
-// and then return the new note to the client.
-let newList = [];
+  });
 
-app.post("/api/notes", function (req, res) {
-
-
-  //   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+  //   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, 
+  // and then return the new note to the client.
 
 
-  // // // let note = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));    
+  app.post("/api/notes", function (req, res) {
+
+
+    //   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+
+
+    // // // let note = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));    
     let newNote = req.body;
     console.log(newNote);
 
+    var x = function y() {
+      (readFileSync('./db/db.json', 'utf8'))
+      // let getFileData = getFile();
+      // console.log(getFileData);
+      .then(noteList => {
+        console.log(noteList)
+        return (JSON.parse(noteList))
+        res.json(JSON.parse(noteList))
+      })
+      .catch(err => console.log(err))
+
+      return (JSON.parse(noteList))
+    }
+
+      (JSON.parse(noteList)).push(newNote);
 
     writeFileSync('./db/db.json', JSON.stringify(newNote, null, 2),
-    "utf8")
+      "utf8")
 
-  // We then add the json the user sent to the notes array
-  // listArray.push(newNote);
-  newList.push(newNote);
-  // We then display the JSON to the users
-  res.json(newNote);
+    // We then add the json the user sent to the notes array
+    // listArray.push(newNote);
+    // newList.push(newNote);
+    // We then display the JSON to the users
+    res.json(newNote);
 
-  
-});
+
+  });
 
 
 }
