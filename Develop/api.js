0031ac45d407d336bac9a337eed7c65149b24e64
WillV1 <wmvanhook@gmail.com>
2020-05-22 22:05:23 -0400

    Uploading latest changes for current delete functionality

--- Develop/api.js
+++ Develop/api.js
@@ -2,24 +2,29 @@ const express = require("express");
 const fs = require("fs");
 const util = require('util');
 
+
 const readFileSync = util.promisify(fs.readFile);
 const writeFileSync = util.promisify(fs.writeFile);
 
 
+
 module.exports = function (app) {
 
-     //   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+  //   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+
 
   app.get("/api/notes", function (req, res) {
 
     (readFileSync('./db/db.json', 'utf8'))
       .then(noteList => {
+
         console.log(noteList, "data-two")
         res.json(JSON.parse(noteList))
       })
       .catch(err => console.log(err))
 
   });
+
 
   //   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, 
   // and then return the new note to the client.
@@ -30,44 +35,54 @@ module.exports = function (app) {
     let newNote = req.body;
     console.log(newNote);
 
-
-    let notes_string = await readFileSync("./db/db.json", "utf8");
-    let notes = JSON.parse(notes_string);
-    notes.push(newNote);
-
-    let { title, text } = notes;
-
     var num = Math.floor(Math.random() * 100 + 1);
     var n = num.toString();
 
     let new_property = { "id": n }
 
-    let new_object = [{ "title":"Note", "text":"Note", ...new_property }]
+    let new_object = [{ "title": req.body.title, "text": req.body.text, ...new_property }]
+    let notes_string = await readFileSync("./db/db.json", "utf8");
 
-    console.log("-----------")
-    console.log(new_object)
-    // console.log(notes)
-    let new_notes = JSON.stringify(new_object)
-    // var new_notes = JSON.stringify(notes)
+    let notes = JSON.parse(notes_string);
+ 
+    notes.push(new_object[0])
+    let new_notes = JSON.stringify(notes)
 
     await writeFileSync('./db/db.json', new_notes, "utf8");
 
     console.log(new_notes, "data");
-
     res.json(new_notes);
 
   });
 
   //   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
-// This means you'll need to find a way to give each note a unique `id` when it's saved. 
-// In order to delete a note, you'll need to read all notes from the `db.json` file, 
-// remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+  // This means you'll need to find a way to give each note a unique `id` when it's saved. 
+  // In order to delete a note, you'll need to read all notes from the `db.json` file, 
+  // remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+  app.delete("/api/notes/:id", async function (req, res){
+
+    let noteID  = req.params.id;
+
+    let full_string = await readFileSync("./db/db.json", "utf8");
+
+    let notes = JSON.parse(full_string);
+
+    // let id = parseInt(id);
+
+    let note = notes.filter(note => {
+      return note.id == noteID;
+    });[0];
+
+    let noteIndex = notes.indexOf(note);
+ 
+    notes.splice(noteIndex, 1);
+
+    let new_notes = JSON.stringify(notes)
+
+    await writeFileSync('./db/db.json', new_notes, "utf8");
+
+    res.send("Item deleted");
+  });
 
 }
-
-// Receive info id of note
-// Read File
-// Parse info
-// Delete instanceof
-// stringify
-// Rewrite
